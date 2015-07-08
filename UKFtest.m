c448a5d404ff27785785475b0f0d231c0d2369ae
DanWu94 <zjlxwudan@gmail.com>
2015-07-08 18:28:26 +0800

    even ground truth not working

--- UKFtest.m
+++ UKFtest.m
@@ -1,16 +1,16 @@
 clear all;close all;clc;
+load data.mat;
 %% Set model parameters
 % A simple movement in 3-d space
 m = [0, 0, 0, 1, 2, 3, ...
     0, 0, 0, 0, 0, 0, ...
-    0, 0, 0, 1, 1, 1]';
+    0, 0, 0, 2, 3, 3]';
 % [O_1, O_2, O_3, Odot_1, Odot_2, Odot_3, 
 % euler_1, euler_2, euler_3, eulerdot_1, eulerdot_2, eulerdot_3,
 % p_1, p_2, p_3, v_1, v_2, v_3]
 n = length(m);
 P = eye(n);
 deltat = 1;
-phi_s = 1;
 C = [eye(3)*deltat^3/3, eye(3)*deltat^2/2; eye(3)*deltat^2/2, eye(3)*deltat];
 Qpi = [C, zeros(size(C)); zeros(size(C)), C];
 sigma_p = 1;
@@ -39,10 +39,10 @@ W = (eye(2*n+1)-repmat(W_m,1,2*n+1))*diag(W_c)*(eye(2*n+1)-repmat(W_m,1,2*n+1))'
 figure;
 ySE = 0;
 mSE = 0;
-for t = 0:100
+for t = 1:size(data,2)
 %% Fake input
-    y = [t, 2*t, 3*t, 0, 0, 0, 0, 0, 0, 1, 1, 1, t, t]'+chol(R)*randn(14,1);
-    plot3(y(1),y(2),y(3),'rx');
+    y = data(:,t);
+    plot3(y(7),y(8),y(9),'rx');
     hold on;
 %% Prediction
     X = repmat(m,1,2*n+1)+sqrt(c)*[zeros(size(m)),chol(P),-chol(P)];
@@ -64,7 +64,7 @@ for t = 0:100
     K = C/S;
     m = mminus + K*(y-mu);
     P = Pminus - K*S*K';
-    plot3(m(1),m(2),m(3),'b.');
+    plot3(m(13),m(14),m(15),'bo');
     hold on;
 end
 
---
+++ fakeData.m
@@ -0,0 +1,33 @@
+clear all;close all;clc;
+% Generate fake data for simulation
+%% Set simulation parameters
+O_0 = [0, 0, 0]';
+Odot = [1, 2, 3]';
+euler = [0, 0, 0]';
+R = R_euler(euler(1), euler(2), euler(3));
+uhat = R(:,1);
+vhat = R(:,2);
+sigma = 1;
+deltat = 1;
+T = 10;
+%% Data generating
+data = zeros(14,100);
+for t = 1:T/deltat
+    data(1:3,t) = O_0 + Odot.*t*deltat;
+    data(4:6,t) = euler;
+    data(13,t) = t*deltat;
+    data(14,t) = t*deltat;
+    data(7:9,t) = data(1:3,t) + data(13,t)*uhat + data(14,t)*vhat;
+    if t == 1
+        data(10:12,t) = data(7:9,t);
+    else
+        data(10:12,t) = data(7:9,t)-data(7:9,t-1);
+    end
+end
+%data = data + sigma*randn(size(data));
+figure;
+for t = 1:T/deltat
+   plot3(data(7,t),data(8,t),data(9,t));
+   hold on;
+end
+save('data.mat','data');
